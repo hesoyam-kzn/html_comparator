43b87cdb4720f1e4b91d689f7dd088084415ff79
hesoyam-kzn <dimaswat@gmail.com>
2025-08-09 15:16:00 +0200

    Third iteration of scripts

--- html_compares.py
+++ html_compares.py
@@ -1,5 +1,6 @@
 import subprocess, os
 from sys import argv
+from html_parser import parse_html
 
 def report_diff(p1, p2):
     winmerge_path = "C:/Program Files/WinMerge/WinMergeU.exe"
@@ -19,21 +20,30 @@ def report_diff(p1, p2):
                 print("Incorrect input, try again.")
 
     if all(".html" in line for line in [p1, p2]):
-        a = subprocess.run([winmerge_path, p1, p2, "/t", "Webpage"])
+        conversion_need = (False if input("\nYou've sent 2 htmls, choose how to work with them:\n"
+                                 "1 \x1b[96m(Default)\x1b[0m - Convert to .json and compare after\n"
+                                 "2 - Compare two htmls\nYour choice: ") == "2" else True)
+        if conversion_need:
+            left, right = parse_html(p1, "report_left.json"), parse_html(p2, "report_right.json")
+            a = subprocess.run([winmerge_path, left, right])
+            return a.returncode
+        else:
+            a = subprocess.run([winmerge_path, p1, p2, "/t", "Webpage"])
+            return a.returncode
     elif all(".json" in line for line in [p1, p2]):
         a = subprocess.run([winmerge_path, p1, p2])
-    #b = subprocess.run([winmerge_path, path1, path2, "/or", "C:/TMP/report.html", "/noninteractive", ])
-    #TODO: ебани ответ по коду возврата если нет никакого диффа, чтобы юзер в комстроке это явно увидел
+        return a.returncode
+    else:
+        input("\x1b[31mERROR\x1b[0m\nExtensions for provided paths were not equal. Expected 2 .html or 2 .json\n"
+              "Example: html_compares.py 'E:/report1.html' 'E:/report2.html'")
+        # return
 
 if __name__ == "__main__":
-    mode_cmd = True
-    if len(argv) < 2:
-        mode_cmd = False
-    if mode_cmd:
+    if len(argv) == 3:
         path1, path2 = argv[1].replace("\\", "/"), argv[2].replace("\\", "/")
-    if mode_cmd:
         b = report_diff(path1, path2)
-    # This text is shown even when you call comparisson from aside. MB there is a way to show it only if html_compares.py is running itself and report_diff is not called from another .py?
-    # else:
-    #     print("Please restart and provide two paths for validation reports. Available income extensions: .html and .json.\n" \
-    #     "Example: html_compares.py 'E:/report1.html' 'E:/report2.html'")
+        print(f'Returncode is {b}')
+    else:
+        input("\x1b[31mERROR\x1b[0m\n Please restart and provide two paths for validation reports with the same extensions.\n"
+              "Available income extensions: .html and .json.\n"
+              "Example: html_compares.py 'E:/report1.html' 'E:/report2.html'")
--- html_parser.py
+++ html_parser.py
@@ -1,23 +1,24 @@
 import json
 from sys import argv
-from html_compares import report_diff
 import os
 
 def parse_html(html_path, parsed_report_path):
     print(f'Processing {html_path}')
-    html_p = html_path
-    header, report_data = "", ""
+    if ".html" not in html_path:
+        input("\nArgument provided is not a .html report. Expected .html. Try again with correct parameter.\n"
+              "Example: html_parser.py 'E:/report1.html'")
+        return
     issue_types = {"Ammounts": {"Errors": 0, "Warnings": 0, "Notices": 0}, "Error": [], "Warning": [], "Notice": []}
-    stats1 = dict()
     issues = []
 
     # Working with passed html path
-    with open(html_p, "r", encoding="utf-8") as inp:
+    with open(html_path, "r", encoding="utf-8") as inp:
         raw_html = inp.read()
         header = raw_html[raw_html.index("<tr>"):raw_html.index("</table>")]
         
         # Gathering issues count from the report's header
-        header = header.replace('<tr><th id="col_info">', '').replace('</td></tr>', '').replace('</th><td>', '').replace('</tbody>', '')
+        header = header.replace('<tr><th id="col_info">', '').replace('</td></tr>', '')\
+            .replace('</th><td>', '').replace('</tbody>', '')
 
         issue_types["Ammounts"]["Errors"] = int(header[13:header.index("Total Warnings:")])
         issue_types["Ammounts"]["Warnings"] = int(header[header.index("Total Warnings:") + 15:header.index("Total Notices:")])
@@ -27,7 +28,8 @@ def parse_html(html_path, parsed_report_path):
         # Gathering all found issues for further processing
         results_start = raw_html.index("<tr>", raw_html.index("Description"))
         results_end = raw_html.index("</html>")
-        report_data = raw_html[results_start:results_end].replace('</tbody>', '').replace('</table>','').replace('</body>', '').rstrip()
+        report_data = raw_html[results_start:results_end].replace('</tbody>', '')\
+            .replace('</table>','').replace('</body>', '').rstrip()
 
         # Separating issues and adding them into issues list
         pointer_start, pointer_end = report_data.index("<tr>") + len("<tr>"), report_data.index("</tr>")
@@ -39,19 +41,22 @@ def parse_html(html_path, parsed_report_path):
                 pointer_end = report_data.index('</tr>', pointer_start)
                 
         # Processing issues and distributing each into an appropriate issue group (Error \ Warning \ Notice)
+        sc_reg = '"col_scenario">'
+        src_reg = '"col_source">'
+        dsc_reg = '"col_description">'
         for issue in issues:
             if "Error" in issue:
-                issue_types["Error"].append({"Scenario": issue[issue.index('"col_scenario">') + 15:issue.index('</td>')],
-                                            "Source": issue[issue.index('"col_source">') + 13:issue.index('</td>', issue.index('"col_source">'))],
-                                            "Description": issue[issue.index('"col_description">') + 18:issue.index('</td>', issue.index('"col_description">'))]})
+                issue_types["Error"].append({"Scenario": issue[issue.index(sc_reg) + 15:issue.index('</td>')],
+                                            "Source": issue[issue.index(src_reg) + 13:issue.index('</td>', issue.index(src_reg))],
+                                            "Description": issue[issue.index(dsc_reg) + 18:issue.index('</td>', issue.index(dsc_reg))]})
             elif "Warning" in issue:
-                issue_types["Warning"].append({"Scenario": issue[issue.index('"col_scenario">') + 15:issue.index('</td>')],
-                                            "Source": issue[issue.index('"col_source">') + 13:issue.index('</td>', issue.index('"col_source">'))],
-                                            "Description": issue[issue.index('"col_description">') + 18:issue.index('</td>', issue.index('"col_description">'))]})
+                issue_types["Warning"].append({"Scenario": issue[issue.index(sc_reg) + 15:issue.index('</td>')],
+                                            "Source": issue[issue.index(src_reg) + 13:issue.index('</td>', issue.index(src_reg))],
+                                            "Description": issue[issue.index(dsc_reg) + 18:issue.index('</td>', issue.index(dsc_reg))]})
             elif "Notice" in issue:
-                issue_types["Notice"].append({"Scenario": issue[issue.index('"col_scenario">') + 15:issue.index('</td>')],
-                                            "Source": issue[issue.index('"col_source">') + 13:issue.index('</td>', issue.index('"col_source">'))],
-                                            "Description": issue[issue.index('"col_description">') + 18:issue.index('</td>', issue.index('"col_description">'))]})
+                issue_types["Notice"].append({"Scenario": issue[issue.index(sc_reg) + 15:issue.index('</td>')],
+                                            "Source": issue[issue.index(src_reg) + 13:issue.index('</td>', issue.index(src_reg))],
+                                            "Description": issue[issue.index(dsc_reg) + 18:issue.index('</td>', issue.index(dsc_reg))]})
             else:
                 print(f"Couldn't distribute {issue} to any of available groups")
 
@@ -66,17 +71,17 @@ def parse_html(html_path, parsed_report_path):
     with open(parsed_report_path, "w", encoding="utf-8") as out2:
         json.dump(issue_types, out2, indent="\t", ensure_ascii=False)
     
-    print(f'Report {html_p} has been processed. Results in {parsed_report_path}. Report folder: {os.getcwd()}')
+    print(f'Report {html_path} has been processed. Results in {parsed_report_path}. Report folder: {os.getcwd()}')
     return parsed_report_path
 
-# if len(argv) > 1:
-#     d1, d2 = argv[1].replace("\\", "/"), argv[2].replace("\\", "/")
-
-if len(argv) > 1:
-    d1 = argv[1].replace("\\", "/")
-
-doc1 = parse_html(d1, "report_left.json")
-# doc2 = parse_html(d2, "report_right.json")
-
-# report_diff(doc1, doc2)
+if __name__ == "__main__":
+    if len(argv) == 2:
+        d1 = argv[1].replace("\\", "/")
+        doc1 = parse_html(d1, "parsed_report.json")
+    elif len(argv) == 1:
+        input("Path to html for conversion was not provided. Try again.\n"
+              "Example: html_parser.py 'E:/report1.html'")
+    else:
+        input("Too many arguments provided. Expected only one.\n"
+              "Example: html_parser.py 'E:/report1.html'")
 
